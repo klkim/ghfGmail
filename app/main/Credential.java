1938f1098416db5d0fa2f83468050df7f6bd0484
Kim, Kwanlae <voidopennet@gmail.com>
2012-10-15 17:04:39 +0900

    Update app/main/Credential.java

--- app/main/Credential.java
+++ app/main/Credential.java
@@ -1,12 +1,11 @@
-
 package main;
 
 
 public class Credential {
-	public static String ID = "personacubic@gmail.com";
-	public static String PW = "11112222";
+	public static String ID = "person@gmail.com";
+	public static String PW = "";
 	
-	public static String PusherAppID = "25487";
-	public static String PusherKey = "95189161bfb8e7276632";
-	public static String PusherSecret = "22f2872cc34a6c407331";		
+	public static String PusherAppID = "";
+	public static String PusherKey = "";
+	public static String PusherSecret = "";		
 }
